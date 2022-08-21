9ea1f524223f9cd49ef13e7b1caf41be0969e826
nikcxprojects@gmail.com <smallgamescompany@gmail.com>
2022-08-21 15:34:08 +0300

    update

--- Assets/Chucha Chupucha/Scripts/PlayerLocomotion.cs
+++ Assets/Chucha Chupucha/Scripts/PlayerLocomotion.cs
@@ -52,34 +52,11 @@ public class PlayerLocomotion : NetworkBehaviour
 
         UpdatePosition(Inputs);
     }
+
     private void UpdatePosition(SoldierInput.NetworkInputData input)
     {
         Vector3 inputNormalized = input.moveDirection.normalized;
         MovementDirection = new Vector3(inputNormalized.x, 0, inputNormalized.y);
         networkCharacterControllerPrototype.Move(MovementDirection * maxStrength);
     }
-
-    //private void LateUpdate()
-    //{
-    //    CameraRotation();
-    //}
-    //private void CameraRotation()
-    //{
-    //    // if there is an input and camera position is not fixed
-    //    if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
-    //    {
-    //        //Don't multiply mouse input by Time.deltaTime;
-    //        float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-
-    //        _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-    //        _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
-    //    }
-
-    //    // clamp our rotations so our values are limited 360 degrees
-    //    _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-    //    _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
-
-    //    // Cinemachine will follow this target
-    //    CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
-    //}
 }
